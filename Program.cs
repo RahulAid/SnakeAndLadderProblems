aa0c817822f48c89c41110fdda438586504e11ad
RahulAid <raccess97@gmail.com>
2022-11-03 22:52:02 +0530

    Check if Player One Moved ahead or Back or Remained at Same Position

--- Program.cs
+++ Program.cs
@@ -10,12 +10,27 @@
             Console.WriteLine("Initial position of Player 1 is {0}",pos);
             Random random = new Random();
             int move = random.Next(1, 7);
-            Console.WriteLine("Player 1 rolled a dice and got a value {0}", move);
-            int newpos = pos + move;
-            Console.WriteLine("Player 1 is now at position {0}", newpos);
+            int wave = random.Next(1, 4);
+            int incpos = pos + move;
+            int decpos = pos - move;
+            switch (wave)
+            {
+                case 1:
+                    Console.WriteLine("Player 1 got a Ladder and a value {0} on Dice", move);
+                    
+                    Console.WriteLine("Player 1 is now at position {0}", incpos);
+                    break;
+                case 2:
+                    Console.WriteLine("Player 1 got a Snake and a value {0} on Dice", move);
 
+                    Console.WriteLine("Player 1 is now at position {0}", decpos);
+                    break;
+                case 3:
+                    Console.WriteLine("Player 1's Move is Unclear and Remains at Position {0}", pos);
+                    break;
 
-
+            }
+            
         }
     }
 }
