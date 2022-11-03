128b995418ad8794842a4be9fab56af2bcc9d3aa
RahulAid <raccess97@gmail.com>
2022-11-03 22:12:59 +0530

    Player One Start Position

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Problems");
-            
+            Console.WriteLine("Let the Snake And Ladder Game Begin");
+            int pos = 0;
+            Console.WriteLine("Player 1 is at position {0}",pos);
+
+
+
         }
     }
 }
