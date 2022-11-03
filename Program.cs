b2172a8380b973a59a10b09540bef04f311556c1
RahulAid <raccess97@gmail.com>
2022-11-03 22:28:26 +0530

    Check New Position of Player One using RANDOM Function

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@
             Console.WriteLine("Welcome to Snake And Ladder Problems");
             Console.WriteLine("Let the Snake And Ladder Game Begin");
             int pos = 0;
-            Console.WriteLine("Player 1 is at position {0}",pos);
+            Console.WriteLine("Initial position of Player 1 is {0}",pos);
+            Random random = new Random();
+            int move = random.Next(1, 7);
+            Console.WriteLine("Player 1 rolled a dice and got a value {0}", move);
+            int newpos = pos + move;
+            Console.WriteLine("Player 1 is now at position {0}", newpos);
 
 
 
